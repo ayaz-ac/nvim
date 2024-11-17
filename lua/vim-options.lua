c92b6afe1cd4419e5fdda630ab49c78bc661314b
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-11-17 16:34:27 +0100

    Update vim-options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,15 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+
+vim.opt.swapfile = false
+
+-- Navitage vim panes better
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
