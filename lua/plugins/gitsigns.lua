7a0a825d122918151cb2b1d205eec9948b4f95bf
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-11-20 08:25:14 +0100

    Add GitSigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,23 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    signs = {
+      add = {
+        text = "",
+      },
+      change = {
+        text = "~",
+      },
+      delete = {
+        text = "",
+      },
+      topdelete = {
+        text = "",
+      },
+      changedelete = {
+        text = "~",
+      },
+    },
+    current_line_blame = true,
+  },
+}
