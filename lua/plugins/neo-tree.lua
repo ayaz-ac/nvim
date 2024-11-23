a525e860b10f6fc36bde2ede222ae2e61935193b
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2024-11-23 08:48:50 +0100

    Update neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,28 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal right<CR>')
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+      window = {
+        position = 'right'
+      },
+			filesystem = {
+				filtered_items = {
+					visible = true, -- when true, they will just be displayed differently than normal items
+					hide_dotfiles = false,
+				},
+				follow_current_file = {
+					enabled = true, -- This will find and focus the file in the active buffer every time the current file is changed while the tree is open.
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+	end,
 }
