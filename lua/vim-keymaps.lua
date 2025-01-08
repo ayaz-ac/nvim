668d0bdbf7a1e7aa6d4c9f0df30e5c1b6548948d
Ayaz <24913980+ayaz-ac@users.noreply.github.com>
2025-01-08 15:11:56 +0100

    Disable macro key

--- lua/vim-keymaps.lua
+++ lua/vim-keymaps.lua
@@ -7,6 +7,8 @@ function keys.init()
   vim.keymap.set("n", "<left>", "<nop>", { silent = true })
   vim.keymap.set("n", "<right>", "<nop>", { silent = true })
 
+  -- Remove macro key since I don't use it
+  vim.keymap.set("n", "q", "<nop>", { silent = true })
   -- Clear search with <esc>
   vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch", silent = true })
 
@@ -25,6 +27,7 @@ function keys.init()
   vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent selected lines", silent = true })
   vim.keymap.set("i", "<Tab>", "<C-t>", { desc = "Indent line in insert mode", silent = true })
   vim.keymap.set("i", "<S-Tab>", "<C-d>", { desc = "Unindent line in insert mode", silent = true })
+
 end
 
 return keys
